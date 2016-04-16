e0c5fb8dee6819b9c314f449e83f1a5f4373ed9f
Joel <johnson.joel.86@gmail.com>
2016-04-16 00:36:42 -0600

    pulling

--- modules/image.py
+++ modules/image.py
@@ -18,6 +18,7 @@ def help_summary(name: str) -> str:
     return "Various commands for images"
 
 
+# noinspection PyUnusedLocal
 def import_command(docker_client: docker.Client, args):
     args.set_defaults(func=default)
     subparsers = args.add_subparsers(title="Container Commands")
@@ -29,7 +30,13 @@ def import_command(docker_client: docker.Client, args):
     list_cmd.add_argument('--pprint', action='store_true', help='Dump contents using python\'s pprint function')
     list_cmd.set_defaults(func=list_images)
 
+    pull_cmd = subparsers.add_parser('pull', help="Pulls an image")
+    pull_cmd.add_argument('image', help="The name of the image to pull")
+    pull_cmd.add_argument('-a', '--all-tags', action='store_true', help='Download all tagged images in the repository')
+    pull_cmd.set_defaults(func=pull_image)
 
+
+# noinspection PyUnusedLocal
 def default(client: docker.Client, args):
     print("No valid command specified. `{} image -h` for help.".format(sys.argv[0]))
 
@@ -81,6 +88,36 @@ def list_images(client: docker.Client, args):
             row.append(_sizeof_fmt(image['VirtualSize']))
 
     print(tabulate(table, headers=headers, tablefmt="plain"))
+
+
+def pull_image(docker_client: docker.Client, args):
+    CURSOR_UP_ONE = '\x1b[1A'
+    ERASE_LINE = '\x1b[2K'
+
+    image = args.image
+
+    if not args.all_tags and ":" not in image:
+        image = "{}:latest".format(image)
+
+    print("Pulling {}".format(image))
+    pull_status_gen = docker_client.pull(image, stream=True)
+
+    previous_layer = ""
+    for pull_status in pull_status_gen:
+        pull_statuses = pull_status.decode('utf8').strip().split("\r\n")
+
+        for ps in pull_statuses:
+            pull_obj = json.loads(ps)
+
+            this_id = pull_obj['id'] if 'id' in pull_obj else ""
+            if previous_layer == this_id:
+                print(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE)
+
+            if 'progress' in pull_obj:
+                print("{} {} {}".format(pull_obj['status'], this_id, pull_obj['progress']))
+            else:
+                print("{} {}".format(pull_obj['status'], this_id))
+            previous_layer = this_id
 
 
 def _sizeof_fmt(num, suffix='B'):
